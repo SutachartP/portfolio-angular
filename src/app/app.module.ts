326cd95d0904342aa836db26e57fd82160ad4cc9
sutachart <sutachart_me@kkumail.com>
2022-05-31 22:42:50 +0700

    add new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './feature/about/about.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProjectComponent } from './feature/project/project.component';
+import { ContactComponent } from './feature/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProjectComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
